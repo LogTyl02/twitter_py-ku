6cb23df115629b92563dea69733ea84122aaf828
LogTyl02 <logtyl02@evergreen.edu>
2013-11-20 00:53:13 -0800

    tested syllable count functionality
    syllables in list/dict comprehension

--- tests/comprehensions.py
+++ tests/comprehensions.py
@@ -1,12 +1,16 @@
+from syllable_test import *
+
 sentence = 'This poor grass-roofed hut of old brushwood may sound miserable, but I very quickly found it altogether suiting my taste.'
 
 def obliterate(line):
 	words = line.split()
-	obliteration = [[w.upper(), w.lower(), len(w)] for w in words]
+	obliteration = [{'Word: ':w.upper(), 'Length: ':len(w), 'Syllables: ':CountSyllables(w)} for w in words]
 
 	return obliteration
 
 # Test
 
-out = obliterate(sentence)
-print out
+shiva = obliterate(sentence)
+
+for i in shiva:
+	print i
